91ea98e01c06926cb8dc2d8416399bdcc1464850
Chico Camargo <chico.camargo@oii.ox.ac.uk>
2020-05-15 18:41:32 +0000

    Added PHP+SQL code to save user information

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `surveys` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+CREATE TABLE IF NOT EXISTS `surveys`.`logging` (
+  `userid` VARCHAR(45) NOT NULL COMMENT '',
+  `section` VARCHAR(45) NOT NULL COMMENT '',
+  `payload` LONGBLOB NULL COMMENT '',
+  `datetime` INT(11) NOT NULL COMMENT '',
+  INDEX `user_section` (`userid` ASC, `section` ASC)  COMMENT '');
+ENGINE = InnoDB;
+DEFAULT CHARACTER SET = utf8;
---
+++ db/save_functions.php
@@ -0,0 +1,71 @@
+<?php
+include ".config.php";
+
+function dblogger ($userid, $section, $payload, $datetime)
+{
+	// Create connection
+	global $servername,$username,$password,$dbname;
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	//echo " servername: " . $servername . ", username: " . $username . ", pw: " . $password . ", dbname: " . $dbname;
+	//echo ", userid: " . $userid . ", section: " . $section . ", payload: " . $payload . ", dt: " . $datetime;
+
+	// Check connection
+	if ($conn->connect_error) {
+		throw new Exception('{"error":"Failed to connect to database: ' . $conn->connect_error . '"}');
+		return $conn->connect_error;
+	}
+
+	// bind parameters and execute
+	$stmt = $conn->prepare("INSERT INTO logging (userid, section, payload, datetime) VALUES (?, ?, ?, ?)");
+
+	$stmt->bind_param('sssi', $userid, $section, $payload, $datetime);
+
+	if (!$stmt->execute()) {
+		throw new Exception('{"error": "Execute failed: (' . $stmt->errno . ') ' . $stmt->error . '"}');
+	}
+
+	$stmt->close();
+	$conn->close();
+
+	return true;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+	$userid=$_POST["user_id"];
+
+	$section=$_POST["section"];
+
+	//$payload=json_encode($_POST["payload"]);
+	$payload=$_POST["payload"];
+
+	$datetime=new DateTime();
+	$datetime=$datetime->getTimestamp();
+
+	dblogger($userid,$section,$payload,$datetime);
+
+}
+elseif ($_SERVER['REQUEST_METHOD'] == 'GET') 
+{
+	/*
+	$email=$_GET["email"];
+	$userid=$_GET["user_id"];
+	$section="email";
+	//$payload=json_encode($_GET);
+	$datetime=new DateTime();
+	$datetime=$datetime->getTimestamp();
+
+	dblogger($userid,$section,$payload,$datetime);
+	*/
+	echo "222";
+}
+else
+{
+	echo "333";
+}
+
+
+?>
